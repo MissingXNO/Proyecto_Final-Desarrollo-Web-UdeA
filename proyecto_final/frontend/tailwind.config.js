edb3b891b1c036499c756d464c6225627d75eb56
MissingXNO <santiago.giraldot@udea.edu.co>
2024-11-15 04:40:38 -0500

    Se avanzó en el estilizado en Tailwind CSS de la página para asemejarse al proyecto plantado inicialmente. Se añadieron imagenes y links, así como los archivos de configuración necesarios para el estilizado

--- proyecto_final/frontend/tailwind.config.js
+++ proyecto_final/frontend/tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./pages/**/*.html", "./assets/js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      opacity: {
+        '0': '0',
+        '100': '1',
+      },
+      transitionProperty: {
+        'opacity': 'opacity',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeOut: 'fadeOut 1s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
